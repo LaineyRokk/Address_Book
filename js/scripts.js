ccaf38564581166398596bc0c7251946cc21e0a1
Lainey Rokkan and mike holland <lainey@>
2017-08-22 16:45:01 -0700

    add pricing function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 
-function Ticket = (movie, time, age, cost)
-
-
+function Ticket(movie, time, age) {
     this.movie = movie;
     this.time = time;
     this.age = age;
-    this.cost = cost;
 }
 
 Ticket.prototype.movieTicket = function () {
-  this.time + this.age) * this.cost;
+  return this.time + this.movie + this.age
 
 }
+
+
+
+$(document).ready(function() {
+  $("#movieTicket").submit(function(event) {
+    event.preventDefault();
+
+    var movieInput = $("#movie").val();
+    var timeInput = $("#time").val();
+    var ageInput = $("#age").val();
+
+    // alert(movieInput);
+    // alert(timeInput);
+    // alert(ageInput);
+
+    var newTicket = new Ticket(parseInt(movieInput), parseInt(timeInput), parseInt(ageInput));
+    alert( newTicket.movieTicket());
+  });
+});
