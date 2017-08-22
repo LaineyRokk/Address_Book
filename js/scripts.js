29c73006cb766fef3fedde2a85398fcf379f4f77
Lainey Rokkan and mike holland <mike@>
2017-08-22 10:34:36 -0700

    add reset fields

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,14 @@ Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 }
 
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+  }
+
 
 // user interface logic
 $(document).ready(function() {
@@ -66,14 +74,13 @@ $(document).ready(function() {
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
+
+
 
   });
 });
